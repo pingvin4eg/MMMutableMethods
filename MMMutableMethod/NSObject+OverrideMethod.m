3604527c797d612bd1ce1a1e730379de3e37277a
Sergey Starukhin <sergey@Mac-mini.local>
2013-02-15 22:02:01 +0400

    add implementation without error handlers

--- MMMutableMethod/NSObject+OverrideMethod.m
+++ MMMutableMethod/NSObject+OverrideMethod.m
@@ -14,8 +14,19 @@
 - (void)overrideMethod:(SEL)selector withBlock:(id)block {
     NSString *newClassName;
     Class newClass;
+    Class trueSelfClass;
     Method methodForOverride;
-    char *trueClassName;
+    IMP oldImplementation;
+
+    trueSelfClass = object_getClass(self);
+    methodForOverride = class_getInstanceMethod(trueSelfClass, selector); // TODO: add error handler if result is NULL
+    // название нового класса формируется из настоящего имени класса на случай если будем скрывать класс переопределяя методы: class и superclass
+    newClassName = [NSString stringWithFormat:@"ShadowClass_%s_%p", class_getName(trueSelfClass), self]; // TODO: add error handler if result is NULL
+    newClass = objc_allocateClassPair(trueSelfClass, [newClassName UTF8String], 0);
+    oldImplementation = class_replaceMethod(newClass, selector, imp_implementationWithBlock(block), method_getTypeEncoding(methodForOverride));
+    // TODO: add error handler if result is NULL
+    objc_registerClassPair(newClass);
+    object_setClass(self, newClass);
 }
 
 @end
